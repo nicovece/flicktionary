89c63de7d5737e13e8505224ce5075a5c6f82000
Nico Vece <me@nicovece.com>
2025-03-21 19:05:51 +0100

    Added body parser, uuid and nodemon. Create dummy data movies and users

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express'),
   morgan = require('morgan'),
   fs = require('fs'),
-  path = require('path');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
 const app = express();
 
@@ -9,6 +11,63 @@ const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a'
 });
+
+// Sample data - 2 users
+let users = [
+  {
+    id: 1,
+    name: 'Paul',
+    favoriteMovies: []
+  },
+  {
+    id: 2,
+    name: 'Nico',
+    favoriteMovies: ['John Wick']
+  }
+];
+
+// Sample data - 3 movies
+let movies = [
+  {
+    Title: 'Iron Man 2',
+    Description:
+      'With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodey (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.',
+    Genre: {
+      Name: 'Sci-fi'
+    },
+    Director: {
+      Name: 'John Favreau'
+    },
+    ImageURL:
+      'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p3546118_p_v10_af.jpg'
+  },
+  {
+    Title: 'Captain America: The Winter Soldier',
+    Description:
+      'After the cataclysmic events in New York with his fellow Avengers, Steve Rogers, aka Captain America (Chris Evans), lives in the nation\'s capital as he tries to adjust to modern times. An attack on a S.H.I.E.L.D. colleague throws Rogers into a web of intrigue that places the whole world at risk. Joining forces with the Black Widow (Scarlett Johansson) and a new ally, the Falcon, Rogers struggles to expose an ever-widening conspiracy, but he and his team soon come up against an unexpected enemy.',
+    Genre: {
+      Name: 'Adventure'
+    },
+    Director: {
+      Name: 'Joe & Anthony Russo'
+    },
+    ImageURL:
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTfP_GN63NrHEjZ4unvJbGE7neVs5W_6SlUmYHscaXhUSHMkce3eYBL8eiDbwRt4e0oCv0n'
+  },
+  {
+    Title: 'John Wick',
+    Description:
+      'John Wick is a former hitman grieving the loss of his true love. When his home is broken into, robbed, and his dog killed, he is forced to return to action to exact revenge.',
+    Genre: {
+      Name: 'Action'
+    },
+    Director: {
+      Name: 'Chad Stahelski'
+    },
+    ImageURL:
+      'https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg'
+  }
+];
 
 // Middleware
 
@@ -24,65 +83,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('We have a problem, something gone wrong!');
 });
 
-// Sample data - 10 movies
-const topMovies = [
-  {
-    title: 'Inception',
-    year: 2010,
-    director: 'Christopher Nolan',
-    genre: 'Action, Adventure, Sci-Fi'
-  },
-  { title: 'The Dark Knight', year: 2008, director: 'Christopher Nolan' },
-  {
-    title: 'Pulp Fiction',
-    year: 1994,
-    director: 'Quentin Tarantino',
-    genre: 'Crime, Drama'
-  },
-  {
-    title: 'The Matrix',
-    year: 1999,
-    director: 'Lana & Lilly Wachowski',
-    genre: 'Action, Sci-Fi'
-  },
-  {
-    title: 'The Shawshank Redemption',
-    year: 1994,
-    director: 'Frank Darabont',
-    genre: 'Drama'
-  },
-  {
-    title: 'Fight Club',
-    year: 1999,
-    director: 'David Fincher',
-    genre: 'Drama'
-  },
-  {
-    title: 'Interstellar',
-    year: 2014,
-    director: 'Christopher Nolan',
-    genre: 'Adventure Drama, Sci-Fi'
-  },
-  {
-    title: 'Forrest Gump',
-    year: 1994,
-    director: 'Robert Zemeckis',
-    genre: 'Drama, Romance'
-  },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-    director: 'Peter Jackson',
-    genre: 'Action, Adventure, Drama'
-  },
-  {
-    title: 'Gladiator',
-    year: 2000,
-    director: 'Ridley Scott',
-    genre: 'Action, Adventure, Drama'
-  }
-];
-
 // GET requests
 app.get('/', (req, res) => {
   let title = '<h1>F L I C K T I O N A R Y</h1>';
@@ -90,12 +90,8 @@ app.get('/', (req, res) => {
   res.send(title + description);
 });
 
-// app.get("/documentation", (req, res) => {
-//   res.sendFile("public/documentation.html", { root: __dirname });
-// });
-
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
 // listen for requests
